3195e0c35b85e1fef625e75c7cb01f377d644d34
Alexandre Franco <avfranco@hotmail.com>
2019-12-08 20:59:30 +0000

    Create dsaTest.java
    New JUnit DSA test class

---
+++ src/test/java/avfranco-br/dsaTest.java
@@ -0,0 +1,20 @@
+package avfranco-br;
+  
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class dsaTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
