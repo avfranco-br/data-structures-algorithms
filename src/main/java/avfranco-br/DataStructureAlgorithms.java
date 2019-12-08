d97d68a71bd398fb21c85a1fa62f4aba25f55c28
Alexandre Franco <avfranco@hotmail.com>
2019-12-08 22:08:23 +0000

    Update and rename Main.java to DataStructureAlgorithms.java

--- src/main/java/avfranco-br/DataStructureAlgorithms.java
+++ src/main/java/avfranco-br/DataStructureAlgorithms.java
@@ -1,3 +1,5 @@
+package avfranco-br;
+
 import java.time.LocalTime;
 import java.util.Random;
 import java.util.ArrayList;
@@ -5,180 +7,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 
-class Main {
-  public static void findNemo(String[] array) {
-    for (int index=0; index < array.length; index++) {
-      if (array[index] == "nemo") {
-        System.out.print("Found NEMO!!!");
-      }
-    }
-  }
-  
-  // BIG O Linear Time function O(n)
-  // Not efficient
-  public static void findNumber(int[] arrayOfNumbers, int myNumber) {
-    for (int index=0; index < arrayOfNumbers.length; index++) {
-      //System.out.println(arrayOfNumbers[index]);
-      if (arrayOfNumbers[index] == myNumber) {
-        System.out.println("My number " + myNumber + " was found at position " + index);
-      }
-    }
-  }
-  
-  // Log all pairs of array [1,2,3,4,5] - BIG O (n^2)
-  // Nested arrays
-  public static void logAllPairsOfArray(int[] arrayOfNumbers) {
-    int sizeArray = arrayOfNumbers.length;
-    for (int index=0; index < sizeArray; index++) {
-      for (int index1=0; index1 < sizeArray; index1++) {
-          System.out.println(arrayOfNumbers[index] + "," + arrayOfNumbers[index1]);
-      }
-    }
-  }
-
-  // Sum all pairs of array [1,2,3,4,5] - BIG O (n^2)
-  // Nested arrays
-  public static void sumAllPairsOfArray(int[] arrayOfNumbers) {
-    for (int number1 : arrayOfNumbers) {
-      for (int number2 : arrayOfNumbers) {
-          System.out.println(number1 + "," + number2 + " = " + (number1+number2));
-      }
-    } 
-  }
-
-  // Check if there are same values in two arrays
-  // Do we have any limit size or the Arrays may get very large?
-  // Arrays are sorted?
-  // IF we have duplicated entries, should only return true or amount of times the same letter were found?
-  // Can we assume always the two parameters are given?
-  // Not Time Complexity efficient
-  // Nested Arrays - BIG O(array1 * array2)
-  // Readable code always - variable names, function names etc.
-  // Can this code be improved? Replace for by for each structure
-  // Modular & Clean code - small bits and pieces
-  public static boolean findEqualEntriesOnArray(String[] array1, String[] array2){
-    
-    // Nested Arrays - O(n^2) - time consuming once arrays get bigger
-    // Space Complexity - O(1) - no new variable created, only the function
-    /*
-    for (char value1 : array1) {
-      for (char value2 : array2) {
-        if (value1 == value2) return true;
-      }
-    }
-    */
-
-    // Time complexity O(array1*array2) - better than O(n^2)
-    // Space complexity O(n) - newArray as a copye of Array1 - consume more memory
-    // Faster solution but heavier 
-    // Separate below code out and create a new function mapArrayToHashMap(arg1)
-    HashMap<String,Integer> newArray = new HashMap<String,Integer>();
-
-    for (String letra : array1) {
-      // Don't need to check if property already exist
-      // HashMap.put cares on benhind
-      newArray.put(letra, new Integer(1)); 
-    }
-    System.out.println(newArray.toString());
-
-    // Create a new function CompareTwoArrays(arg1, arg2)
-    for (String letra : array2) {
-      if (newArray.containsKey(letra) == true) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  // Asking - check in a given array has pairs with the sum provided
-  // [1,2,3,4] - sum 8 
-  // [1,2,3,4,4] - sum 8
-  // Are the inputs always the Array and the Sum? 
-  // What need to be returned, true if found, false if not, or the pairs of numbers?
-  // The array contains only integer numbers?
-  // Will be the array already sorted?
-  // Can the array become very large
-  // Is there any priority in terms of time or space complexity?
-  // Can I sum up each item with itself
-  // Can we have duplicated entries?
-  public static boolean hasPairsWithSums(int[] ArrayOfNumbers, int sum) {
-    // FIRST Option would a nested loop
-      // add to the current value of the outer loop + with the subsequent values until find the desired sum or end of the Array 
-      // follow this above steps until outer.length - 2
-      // It solves the problem but it's not efficient, time consuming (O^2), mainly in case the array becomes very large
-      // ArrayOfNumbers [1,2,3,4,4] = i + (i+1)
-      /* for (int i=0; i < ArrayOfNumbers.length-1; i++) {
-        for (int j=i+1; j < ArrayOfNumbers.length; j++) {
-          System.out.println (ArrayOfNumbers[i] + "," + ArrayOfNumbers[j]);
-          if ((ArrayOfNumbers[i]+ArrayOfNumbers[j]) == sum) {
-            return true;
-          }
-        }
-      } 
-      return false; */
-
-    // SECOND Option
-      // First loop convert the Array to HashMap where each value as a Key
-      // Second loop to look for the complements
-      // Same time complexity than the nested loop - O(ArrayOfNumber*newArrayOfNumbers)
-      // ArrayOfNumbers [1,2,3,4,4] --> HashMap [1,2,3,4,4]
-      /*
-      HashMap <Integer,Integer>newArrayOfNumbers = new HashMap<Integer,Integer>();
-      for (int value : ArrayOfNumbers) {
-        newArrayOfNumbers.put (new Integer(value),new Integer(1));
-      }
-
-      for (int value: ArrayOfNumbers) {
-        if (value != (sum - value)) {
-          if (newArrayOfNumbers.containsKey(sum-value)) {
-              return true;
-          }
-        }
-      }
-      return false;
-      */
-
-    // THIRD Option
-    // ArrayOfNumbers [1,2,3,4,4]
-    // SUM fist + last == sum?
-    // More time complexity efficient - O(n)
-    if ((ArrayOfNumbers[0]+ArrayOfNumbers[ArrayOfNumbers.length-1]) == sum) {
-      return true;
-    }
-   
-    // ArrayOfComplements[7,6,5,4]
-    HashMap <Integer,Integer> ArrayOfComplements = new HashMap<Integer,Integer>();
-
-    // Walk-through each ArrayOfNumbers elements and look for its complement (sum - value)
-    for (int value : ArrayOfNumbers) {
-      // sum 9
-      // value [1,2,3,4,4]
-      // ArrayOfComplements [7,6,5,4]
-      if (ArrayOfComplements.containsKey(sum-value)) {
-        return true;
-      }
-      else {
-        ArrayOfComplements.put(new Integer(value), new Integer(1));
-      }
-    }
-
-    return false;
-  }
-
-  public static void TryArrayOperations(ArrayList array) {
-    // Show original array
-    System.out.println(array.toString());
-    //Add a new item
-    array.add(10);
-    // Show new array
-    System.out.println(array.toString());
-    // Sort the Array
-    Collections.sort(array);
-    // Show Sorted array
-    System.out.println(array.toString());
-  }
-  
-  public static void main(String[] args) {
+public class DataStructuresAlgorithms {
+  public static Main(String[] args) {
     //String[] nemo = {"nemo", "flora", "eva"};
     //findNemo(nemo);
     
@@ -400,5 +230,177 @@ class Main {
     }
 
     myBST.stringfy(); // vertex 20 not being printed after vertex 9 deleted
+  }
+  
+  public static void findNemo(String[] array) {
+    for (int index=0; index < array.length; index++) {
+      if (array[index] == "nemo") {
+        System.out.print("Found NEMO!!!");
+      }
+    }
+  }
+  
+  // BIG O Linear Time function O(n)
+  // Not efficient
+  public static void findNumber(int[] arrayOfNumbers, int myNumber) {
+    for (int index=0; index < arrayOfNumbers.length; index++) {
+      //System.out.println(arrayOfNumbers[index]);
+      if (arrayOfNumbers[index] == myNumber) {
+        System.out.println("My number " + myNumber + " was found at position " + index);
+      }
+    }
+  }
+  
+  // Log all pairs of array [1,2,3,4,5] - BIG O (n^2)
+  // Nested arrays
+  public static void logAllPairsOfArray(int[] arrayOfNumbers) {
+    int sizeArray = arrayOfNumbers.length;
+    for (int index=0; index < sizeArray; index++) {
+      for (int index1=0; index1 < sizeArray; index1++) {
+          System.out.println(arrayOfNumbers[index] + "," + arrayOfNumbers[index1]);
+      }
+    }
+  }
+
+  // Sum all pairs of array [1,2,3,4,5] - BIG O (n^2)
+  // Nested arrays
+  public static void sumAllPairsOfArray(int[] arrayOfNumbers) {
+    for (int number1 : arrayOfNumbers) {
+      for (int number2 : arrayOfNumbers) {
+          System.out.println(number1 + "," + number2 + " = " + (number1+number2));
+      }
+    } 
+  }
+
+  // Check if there are same values in two arrays
+  // Do we have any limit size or the Arrays may get very large?
+  // Arrays are sorted?
+  // IF we have duplicated entries, should only return true or amount of times the same letter were found?
+  // Can we assume always the two parameters are given?
+  // Not Time Complexity efficient
+  // Nested Arrays - BIG O(array1 * array2)
+  // Readable code always - variable names, function names etc.
+  // Can this code be improved? Replace for by for each structure
+  // Modular & Clean code - small bits and pieces
+  public static boolean findEqualEntriesOnArray(String[] array1, String[] array2){
+    
+    // Nested Arrays - O(n^2) - time consuming once arrays get bigger
+    // Space Complexity - O(1) - no new variable created, only the function
+    /*
+    for (char value1 : array1) {
+      for (char value2 : array2) {
+        if (value1 == value2) return true;
+      }
+    }
+    */
+
+    // Time complexity O(array1*array2) - better than O(n^2)
+    // Space complexity O(n) - newArray as a copye of Array1 - consume more memory
+    // Faster solution but heavier 
+    // Separate below code out and create a new function mapArrayToHashMap(arg1)
+    HashMap<String,Integer> newArray = new HashMap<String,Integer>();
+
+    for (String letra : array1) {
+      // Don't need to check if property already exist
+      // HashMap.put cares on benhind
+      newArray.put(letra, new Integer(1)); 
+    }
+    System.out.println(newArray.toString());
+
+    // Create a new function CompareTwoArrays(arg1, arg2)
+    for (String letra : array2) {
+      if (newArray.containsKey(letra) == true) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Asking - check in a given array has pairs with the sum provided
+  // [1,2,3,4] - sum 8 
+  // [1,2,3,4,4] - sum 8
+  // Are the inputs always the Array and the Sum? 
+  // What need to be returned, true if found, false if not, or the pairs of numbers?
+  // The array contains only integer numbers?
+  // Will be the array already sorted?
+  // Can the array become very large
+  // Is there any priority in terms of time or space complexity?
+  // Can I sum up each item with itself
+  // Can we have duplicated entries?
+  public static boolean hasPairsWithSums(int[] ArrayOfNumbers, int sum) {
+    // FIRST Option would a nested loop
+      // add to the current value of the outer loop + with the subsequent values until find the desired sum or end of the Array 
+      // follow this above steps until outer.length - 2
+      // It solves the problem but it's not efficient, time consuming (O^2), mainly in case the array becomes very large
+      // ArrayOfNumbers [1,2,3,4,4] = i + (i+1)
+      /* for (int i=0; i < ArrayOfNumbers.length-1; i++) {
+        for (int j=i+1; j < ArrayOfNumbers.length; j++) {
+          System.out.println (ArrayOfNumbers[i] + "," + ArrayOfNumbers[j]);
+          if ((ArrayOfNumbers[i]+ArrayOfNumbers[j]) == sum) {
+            return true;
+          }
+        }
+      } 
+      return false; */
+
+    // SECOND Option
+      // First loop convert the Array to HashMap where each value as a Key
+      // Second loop to look for the complements
+      // Same time complexity than the nested loop - O(ArrayOfNumber*newArrayOfNumbers)
+      // ArrayOfNumbers [1,2,3,4,4] --> HashMap [1,2,3,4,4]
+      /*
+      HashMap <Integer,Integer>newArrayOfNumbers = new HashMap<Integer,Integer>();
+      for (int value : ArrayOfNumbers) {
+        newArrayOfNumbers.put (new Integer(value),new Integer(1));
+      }
+
+      for (int value: ArrayOfNumbers) {
+        if (value != (sum - value)) {
+          if (newArrayOfNumbers.containsKey(sum-value)) {
+              return true;
+          }
+        }
+      }
+      return false;
+      */
+
+    // THIRD Option
+    // ArrayOfNumbers [1,2,3,4,4]
+    // SUM fist + last == sum?
+    // More time complexity efficient - O(n)
+    if ((ArrayOfNumbers[0]+ArrayOfNumbers[ArrayOfNumbers.length-1]) == sum) {
+      return true;
+    }
+   
+    // ArrayOfComplements[7,6,5,4]
+    HashMap <Integer,Integer> ArrayOfComplements = new HashMap<Integer,Integer>();
+
+    // Walk-through each ArrayOfNumbers elements and look for its complement (sum - value)
+    for (int value : ArrayOfNumbers) {
+      // sum 9
+      // value [1,2,3,4,4]
+      // ArrayOfComplements [7,6,5,4]
+      if (ArrayOfComplements.containsKey(sum-value)) {
+        return true;
+      }
+      else {
+        ArrayOfComplements.put(new Integer(value), new Integer(1));
+      }
+    }
+
+    return false;
+  }
+
+  public static void TryArrayOperations(ArrayList array) {
+    // Show original array
+    System.out.println(array.toString());
+    //Add a new item
+    array.add(10);
+    // Show new array
+    System.out.println(array.toString());
+    // Sort the Array
+    Collections.sort(array);
+    // Show Sorted array
+    System.out.println(array.toString());
   }
 }
